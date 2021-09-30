6d9746ece06331467c1f858e44048829c980e72b
Ivan Cheremisin <fess932@gmail.com>
2021-09-30 08:52:50 +0300

    ...

--- app/forum/handlers.go
+++ app/forum/handlers.go
@@ -1,8 +1,10 @@
 package forum
 
 import (
+	"encoding/json"
 	"fmt"
 	"forumless/app/models"
+	"log"
 	"net/http"
 )
 
@@ -10,8 +12,18 @@ func (f Forum) MainHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to forum %s", f.Name)
 }
 
+type postReq struct {
+	Text string
+}
+
 func (f Forum) CreatePostHandler(w http.ResponseWriter, r *http.Request) {
-	f.CreatePost(models.User{}, models.Post{})
+	var p postReq
+
+	if err := json.NewDecoder(r.Body).Decode(&p); err != nil {
+		log.Fatalln(err)
+	}
+
+	f.CreatePost(models.User{}, models.Post{Text: p.Text})
 }
 
 func (f Forum) CreateUserHandler(w http.ResponseWriter, r *http.Request) {
